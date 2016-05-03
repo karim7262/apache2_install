ca2fef2be7e511de76bae88d4b4099705314943a
ANDREY KARPOV <karpovan@gmail.com>
2016-05-04 00:35:05 +0300

    Check for error in update
    Добавлена проверка на наличие ошибок при прохождении тихого обновления

--- installapache.sh
+++ installapache.sh
@@ -6,13 +6,27 @@ function cleardown (){
 cleardown
 ME="$(echo $0 | sed -e 's/^.*\///')"
 if ! [ $(id -u) = 0 ]; then
-        echo -e "Необходимо запустить "$ME" от имени root\\nДля этого выполните команду sudo "$ME
-        exit
+	echo -e "Необходимо запустить "$ME" от имени root\\nДля этого выполните команду sudo "$ME
+	exit
 fi
 
 echo -n "Проверка доступности новых версий пакетов"
-apt-get update --fix-missing > /dev/null 2>&1
-echo -e "\e[0;31m   [Готово]\e[0m"
+apt-get update --fix-missing > /dev/null 2>.update.err
+if ! [ "$?" -eq 0 ]; then
+	echo
+	echo -en "\e[0;31m"
+	cat .update.err | grep -E "^E:"
+	echo -en "\e[0m"
+	rm -f .update.err
+	read -p "Продолжить установку? [Y/N]: " -n 1 
+	case $ANWER in
+		Y|y) cleardown
+			echo -e "\e[0;32mПродолжаем...\e[0m" ;;
+		*) exit 1 ;;
+	esac
+else
+	echo -e "\e[0;32m   [Готово]\e[0m"
+fi
 echo 
 echo "Установка WEB Сервера Apache и включение необходимых модулей"
 apt-get -y install apache2 apache2-utils > /dev/null 2>&1
@@ -33,42 +47,42 @@ fi
 
 cat > /etc/apache2/sites-available/000-default.conf << EOF
 <VirtualHost *:80>
-        # The ServerName directive sets the request scheme, hostname and port that
-        # the server uses to identify itself. This is used when creating
-        # redirection URLs. In the context of virtual hosts, the ServerName
-        # specifies what hostname must appear in the request's Host: header to
-        # match this virtual host. For the default virtual host (this file) this
-        # value is not decisive as it is used as a last resort host regardless.
-        # However, you must set it for any further virtual host explicitly.
-        # ServerName www.example.com
-
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/html
-        AddDefaultCharset utf-8
-		
-        <Directory /var/www/html>
-                Options FollowSymLinks
-                AllowOverride All
-                Order deny,allow
-                Allow From All
-                Require all granted
-        </Directory>
-        
-        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-        # error, crit, alert, emerg.
-        # It is also possible to configure the loglevel for particular
-        # modules, e.g.
-        #LogLevel info ssl:warn
-
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-        # For most configuration files from conf-available/, which are
-        # enabled or disabled at a global level, it is possible to
-        # include a line for only one particular virtual host. For example the
-        # following line enables the CGI configuration for this host only
-        # after it has been globally disabled with "a2disconf".
-        #Include conf-available/serve-cgi-bin.conf
+    # The ServerName directive sets the request scheme, hostname and port that
+    # the server uses to identify itself. This is used when creating
+    # redirection URLs. In the context of virtual hosts, the ServerName
+    # specifies what hostname must appear in the request's Host: header to
+    # match this virtual host. For the default virtual host (this file) this
+    # value is not decisive as it is used as a last resort host regardless.
+    # However, you must set it for any further virtual host explicitly.
+    # ServerName www.example.com
+    
+            ServerAdmin webmaster@localhost
+            DocumentRoot /var/www/html
+            AddDefaultCharset utf-8
+    		
+            <Directory /var/www/html>
+                    Options FollowSymLinks
+                    AllowOverride All
+                    Order deny,allow
+                    Allow From All
+                    Require all granted
+            </Directory>
+            
+    # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+    # error, crit, alert, emerg.
+    # It is also possible to configure the loglevel for particular
+    # modules, e.g.
+    #LogLevel info ssl:warn
+    
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    
+    # For most configuration files from conf-available/, which are
+    # enabled or disabled at a global level, it is possible to
+    # include a line for only one particular virtual host. For example the
+    # following line enables the CGI configuration for this host only
+    # after it has been globally disabled with "a2disconf".
+    #Include conf-available/serve-cgi-bin.conf
 </VirtualHost>
 
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
